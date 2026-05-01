117243a564971568da8b0cdb57bc4e32f1870703
TruongIKPK <phitruong20192020@gmail.com>
2026-05-01 17:05:01 +0700

    feat: integrate dotenv for environment variable management, add JWT verification with public key, and remove unused DTOs

--- src/main/java/com/hamtech/bookstoreapigateway/BookstoreApiGatewayApplication.java
+++ src/main/java/com/hamtech/bookstoreapigateway/BookstoreApiGatewayApplication.java
@@ -1,5 +1,6 @@
 package com.hamtech.bookstoreapigateway;
 
+import com.hamtech.bookstoreapigateway.config.DotenvBootstrap;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BookstoreApiGatewayApplication {
 
     public static void main(String[] args) {
+        DotenvBootstrap.loadDotenvToSystemProperties();
         SpringApplication.run(BookstoreApiGatewayApplication.class, args);
     }
 
